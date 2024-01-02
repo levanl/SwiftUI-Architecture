ddb0b4b08471d6caf4296f2d89ce83a8f6fb5ae1
Levan Loladze <levan.loladzew@gmail.com>
2024-01-02 17:17:06 +0400

    first commit

---
+++ SwiftUI-Architecture/SwiftUI-Architecture/SwiftUI_ArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ArchitectureApp.swift
+//  SwiftUI-Architecture
+//
+//  Created by Levan Loladze on 02.01.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
